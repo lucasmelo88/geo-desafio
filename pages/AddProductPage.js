c2745131bd7903fc6b4dd2274b945a0618a750f6
Lucas Melo <l.melo88@gmail.com>
2017-06-15 14:21:15 -0300

    teste de cadastro de produto com sucesso e melhorias

--- pages/AddProductPage.js
+++ pages/AddProductPage.js
@@ -6,8 +6,10 @@ var AddProductPage = function(){
   var inputName = element(by.id("campo1"));
   var inputPrice = element(by.name("price"));
   var expirationDatePicker = element(by.id("campo3"));
-  var buttonSave = element(by.xpath("//button[@ng-click='addProduct()']"));
+  var buttonSave = element(by.buttonText("Salvar"));
   var buttonCancel = element(by.linkText("Cancelar"));
+  var dateElement = element(by.xpath("/html/body/div[3]/div[1]/table/tbody/tr[5]/td[6]"));  
+  var alertDialog;
 
   this.setName = function(name){
     inputName.sendKeys(name);
@@ -19,10 +21,24 @@ var AddProductPage = function(){
 
   this.setExpirationDate = function(){
     expirationDatePicker.click();
+    browser.sleep(3000);
+    dateElement.click();
   }
 
   this.buttonSaveClick = function(){
     buttonSave.click();
+  }
+
+  this.alertNewProductHandle = function(){
+    alertDialog = browser.switchTo().alert();
+    alertDialog.getText().then(function(text){
+      console.log(text);
+    });
+    alertDialog.accept();
+  }
+
+  this.getMessageAlert = function(){
+    return alertDialog.getText();
   }
 
 };
--- test_spec/AddProductPageTest_spec.js
+++ test_spec/AddProductPageTest_spec.js
@@ -3,16 +3,31 @@ describe("Geofusion Add Product Functionality",function() {
   var home_page = require('../pages/HomePage.js');
   var add_product = require('../pages/AddProductPage.js');
 
-  it("Add new product", function() {
+  beforeEach(function() {
+    //Opening the url
     browser.manage().window().maximize();
     browser.get("http://desafio.geofusion.tech/");
+    //Login
     home_page.setName('Lucas Santana Melo');
     home_page.buttonOkClick();
     browser.sleep(3000);
+  });
+
+  afterEach(function() {
+		browser.sleep(3000);
+    browser.close();
+	});
+
+  it("Add new product with success", function() {
     home_page.buttonAddProductClick();
     add_product.setName('Carne');
     add_product.setPrice('1000');
+    add_product.setExpirationDate();
+    browser.sleep(3000);
     add_product.buttonSaveClick();
+    browser.sleep(3000);
+    add_product.alertNewProductHandle();
+    expect(add_product.getMessageAlert()).toEqual("Produto adicionado com sucesso!");
   });
 
 });
